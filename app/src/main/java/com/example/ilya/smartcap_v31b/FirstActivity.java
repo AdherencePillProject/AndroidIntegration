52b5dcc7eb2af86fb31e4c3344b31c69fc02d779
Dauphin-NWU <seu.wuqihong@gmail.com>
2016-07-12 23:11:31 -0500

    Import FirstActivity

---
+++ app/src/main/java/com/example/ilya/smartcap_v31b/FirstActivity.java
@@ -0,0 +1,29 @@
+package com.example.ilya.smartcap_v31b;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dauphin on 7/12/2016.
+ */
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(FirstActivity.this, MainActivity.class);
+                FirstActivity.this.startActivity(intent);
+            }
+        }, 2000);
+
+    }
+}
